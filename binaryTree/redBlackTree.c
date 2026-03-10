7bbae800ba669de47a47d6ec7072421b6eeadfa7
shantanubeniwal <beniwalhappy07@gmail.com>
2026-03-10 10:31:05 +0530

    added insert fixup function

--- binaryTree/redBlackTree.c
+++ binaryTree/redBlackTree.c
@@ -11,8 +11,8 @@ struct node{
 };
 
 struct RedBlackTree {
-    struct Node* root;
-    struct Node* NIL; // Pointer to the shared sentinel
+    struct node* root;
+    struct node* NIL; // Pointer to the shared sentinel
 };
 
 struct node* createNode(struct RedBlackTree* tree, int data){
@@ -26,40 +26,41 @@ struct node* createNode(struct RedBlackTree* tree, int data){
     return newNode;
 }
 
-void leftRotate(struct RedBlackTree tree, struct node* x){
+void leftRotate(struct RedBlackTree* tree, struct node* x){
     struct node *y = x->right;
     x->right = y->left;
-    if (y->left != tree.NIL)
+    if (y->left != tree->NIL)
     {
         y->left->parent = x;
     }
     y->parent = x->parent;
 
-    if (x->parent == tree.NIL)
+    if (x->parent == tree->NIL)
     {
-        tree.root = y;
+        tree->root = y;
     }else if (x == x->parent->left)
     {
         x->parent->left = y;
     }else{
         x->parent->right = y;
     }
-    y->right = x;
+    y->left = x;
     x->parent = y;
 }
 
-void rightRotate(struct RedBlackTree tree, struct node* x){
+void rightRotate(struct RedBlackTree* tree, struct node* x){
     struct node *y = x->left;
     x->left = y->right;
 
-    if (y->right != tree.NIL)
+    if (y->right != tree->NIL)
     {
         y->right->parent = x;
     }
+    y->parent = x->parent;
     
-    if (x->parent == tree.NIL)
+    if (x->parent == tree->NIL)
     {
-        tree.root = y;
+        tree->root = y;
     }else if (x == x->parent->left)
     {
         x->parent->left = y;
@@ -70,12 +71,75 @@ void rightRotate(struct RedBlackTree tree, struct node* x){
     x->parent = y;
 }
 
-void insertFixup(struct RedBlackTree* tree, struct node* z){
-    while (z->parent->color == RED)
+void insertFixup(struct RedBlackTree* tree, struct node* newNode){
+    struct node *u;  //UNCLE
+    
+    while (newNode->parent != tree->NIL && newNode->parent->color == RED)
     {
+        struct node *p = newNode->parent;  //PARENT
+        struct node *g = p->parent;  //GRAND-PARENT
+        // Case A: Parent is the LEFT child of the Grandparent
+        if (p == g->left)
+        {
+            u = g->right; //uncle is left child of grandparent
+
+            //uncle is red
+            if (u != tree->NIL && u->color == RED)
+            {
+                u->color = p->color = BLACK;
+                g->color = RED;
+                newNode = g;
+            }
+            // uncle is black
+            else
+            {
+                // zig-zag formation
+                if (newNode == p->right)
+                {
+                    newNode = p;
+                    leftRotate(tree, newNode);
+                    p = newNode->parent;
+                    g = p->parent;
+                }
+                // straight line formation on g's left side
+                g->color = RED;
+                p->color = BLACK;
+                rightRotate(tree, g);
+            }
+            
+
+        }
+        // Case B: Parent is the RIGHT child of the Grandparent
+        else{
+            u = g->left;  //uncle is right child of grandparent
+            // uncle is red
+            if (u != tree->NIL && u->color == RED)
+            {
+                u->color = p->color = BLACK;
+                g->color = RED;
+                newNode = g;
+            }
+            // uncle is black
+            else
+            {
+                // zig-zag formationn
+                if (newNode == p->left)
+                {
+                    newNode = p;
+                    rightRotate(tree, newNode);
+                    p = newNode->parent;
+                    g = p->parent;
+                }
+                // straight line formation on g's right side
+                p->color = BLACK;
+                g->color = RED;
+                leftRotate(tree, g);
+            }
+            
+        }
         
     }
-    
+    tree->root->color = BLACK;
 }
 
 // Insert a node into the red-black tree
@@ -109,16 +173,41 @@ void insert(struct RedBlackTree* tree, int data){
     insertFixup(tree, newNode);
 }
 
-    int main()
+// Helper function to print the tree in-order
+void printInOrder(struct RedBlackTree* tree, struct node* root) {
+    if (root != tree->NIL) {
+        printInOrder(tree, root->left);
+        
+        // Print the node's data and its color
+        printf("%d (%s) \n", root->data, root->color == RED ? "RED" : "BLACK");
+        
+        printInOrder(tree, root->right);
+    }
+}
+
+int main()
 {
     struct node sentinel;
     sentinel.color = BLACK;
     sentinel.data = 0;
     sentinel.left = NULL;
     sentinel.right = NULL;
+    sentinel.parent = NULL;
 
     struct RedBlackTree tree;
     tree.NIL = &sentinel;
     tree.root = tree.NIL;
+
+    insert(&tree, 10);
+    insert(&tree, 20);
+    insert(&tree, 30);
+
+    insert(&tree, 15);
+    insert(&tree, 75);
+    insert(&tree, 45);
+    insert(&tree, 65);
+
+    printf("Red-Black Tree In-Order Traversal:\n");
+    printInOrder(&tree, tree.root);
     return 0;
 }
